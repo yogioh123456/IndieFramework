ec79b91c3b682992d52f955bf3e6686977c956b4
周陶顺 <zhoutaoshun@xmfunny.com>
2022-06-02 11:28:39 +0800

    热更框架

---
+++ Assets/.HotScripts/HotFix_Project~/Entry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HotFix_Project
+{
+    public static class Entry
+    {
+        public static void Start()
+        {
+			try
+			{
+				Debug.Log("IDFramework");
+				CodeLoader.Instance.Update += Game.Update;
+				CodeLoader.Instance.FixedUpdate += Game.FixedUpdate;
+				CodeLoader.Instance.LateUpdate += Game.LateUpdate;
+				CodeLoader.Instance.OnApplicationQuit += Game.Close;
+			}
+			catch (Exception e)
+			{
+				Debug.LogException(e);
+			}
+		}
+    }
+}
---
+++ Assets/.HotScripts/HotFix_Project~/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HotFix_Project
+{
+    public class Game
+    {
+        public void Start()
+        {
+            Debug.Log("开始");
+        }
+
+        public static void Update()
+        {
+            Debug.Log("Update");
+        }
+
+        public static void FixedUpdate()
+        {
+            Debug.Log("FixedUpdate");
+        }
+
+        public static void LateUpdate()
+        {
+            Debug.Log("LateUpdate");
+        }
+
+        public static void Close()
+        {
+
+        }
+    }
+}
